5c1fe573667f2c4da0071809873d845ec95bf98e
Nastasja <nastasja@live-eo.com>
2021-05-12 14:46:15 +0200

    added some simple eopatch meta data and raster definition stuff

---
+++ core/metadata_eopatch.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 12 14:07:28 2021
+
+@author: nasta
+"""
+
+def add_metadata_to_eopatch(eopatch, metainfo):
+    eopatch.meta_info[str(metainfo)] = metainfo
+    return eopatch
+
+def get_eopatch_size(eopatch):
+    return eopatch.meta_info.get('size_x'), eopatch.meta_info.get('size_y')
+
+def get_eopatch_bbox(eopatch):
+    return eopatch.bbox
+
+
---
+++ core/raster_definitions.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 12 14:26:49 2021
+
+@author: nasta
+"""
+
+import collections
+import numpy as np
+from eolearn.core import EOPatch, EOTask, FeatureType, FeatureTypeSet
+
+RasterType = collections.namedtuple('RasterType', 
+                                    'id unit sample_type np_dtype feature_type')
+
+
+PREDEFINED_S2_TYPES = {
+    RasterType("bool_mask", 'DN', 'UINT8', bool, FeatureType.MASK): [
+            "dataMask"
+    ],
+    RasterType("mask", 'DN', 'UINT8', np.uint8, FeatureType.MASK): [
+            "CLM", "SCL"
+    ],
+    RasterType("uint8_data", 'DN', 'UINT8', np.uint8, FeatureType.DATA): [
+            "SNW", "CLD", "CLP"
+    ],
+    RasterType("bands", 'DN', 'UINT16', np.uint16, FeatureType.DATA): [
+            "B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08", "B8A", "B09", "B10", "B11", "B12", "B13"
+    ],
+    RasterType("other", 'REFLECTANCE', 'FLOAT32', np.float32, FeatureType.DATA): [
+            "sunAzimuthAngles", "sunZenithAngles", "viewAzimuthMean", "viewZenithMean"
+    ]
+}
+
+
