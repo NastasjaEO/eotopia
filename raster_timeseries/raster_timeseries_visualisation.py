b9f69e590e44a0a80d0f101b894fa90c61d85dd8
Nastasja <nastasja@live-eo.com>
2021-05-14 10:41:32 +0200

    started working on gif visualization of raster timeseries but still needs timestamps for rasterbands

---
+++ raster_timeseries/raster_timeseries_visualisation.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 14 10:27:34 2021
+
+@author: freeridingeo
+"""
+
+import numpy as np
+from eolearn.core import EOPatch
+from .rasterio_IO import save_multibandraster_to_eopatch as saveit
+import imageio
+
+
+def create_rastertimeseries_gif(rasterpath, outpath,
+                                dataname="raster_timeseries",
+                                duration=4):
+    """ 
+    rasterpath  str
+    outpath     str
+    dataname    str
+    duration    GIF duration in seconda
+                int
+    """
+    eopatch = saveit(rasterpath, dataname)
+    fps = len(eopatch.timestamp)/duration
+    
+    with imageio.get_writer(outpath, mode='I', fps=fps) as writer:
+        for image in eopatch:
+            writer.append_data(np.array(image, dtype=np.uint8))
+
