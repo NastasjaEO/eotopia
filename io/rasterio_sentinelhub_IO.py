f9c040ed33f8879c285c387465507039c3cbec7d
Nastasja <nastasja@live-eo.com>
2021-05-19 14:12:52 +0200

    start working on sentinelhub IO stuff adding evalscript class

--- io/rasterio_sentinelhub_IO.py
+++ io/rasterio_sentinelhub_IO.py
@@ -21,6 +21,7 @@ import sys
 sys.path.append("D:/Code/eotopia/repo_core")
 from eodata import EOPatch
 from eotask import EOTask
+from constants import FeatureType, FeatureTypeSet
 
 LOGGER = logging.getLogger(__name__)
 
@@ -216,4 +217,169 @@ class SentinelHubInputBase(EOTask):
         """ 
         Get the timestamp array needed as a parameter for downloading the images
         """
+
+ProcApiType = collections.namedtuple('ProcApiType', 
+                                     'id unit sample_type np_dtype feature_type')
+
+class SentinelHubEvalscriptTask(SentinelHubInputBase):
+    """ 
+    Process API task to download data using evalscript
+    """
+    def __init__(self, features=None, evalscript=None, data_collection=None, 
+                 size=None, resolution=None, maxcc=None, time_difference=None, 
+                 cache_folder=None, max_threads=None, config=None, 
+                 mosaicking_order=None, aux_request_args=None):
+        """
+        :param features: Features to construct from the evalscript.
+        :param evalscript: Evascript for the request. Beware that all outputs 
+            from SentinelHub services should be named
+            and should have the same name as corresponding feature
+        :type evalscript: str
+        :param data_collection: Source of requested satellite data.
+        :type data_collection: DataCollection
+        :param size: Number of pixels in x and y dimension.
+        :type size: tuple(int, int)
+        :type resolution: Resolution in meters, passed as a single number or a 
+            tuple of two numbers -
+            resolution in horizontal and resolution in vertical direction.
+        :type resolution: float or (float, float)
+        :param maxcc: Maximum cloud coverage, a float in interval [0, 1]
+        :type maxcc: float
+        :param time_difference: Minimum allowed time difference, used when 
+            filtering dates, None by default.
+        :type time_difference: datetime.timedelta
+        :param cache_folder: Path to cache_folder. If set to None (default) 
+            requests will not be cached.
+        :type cache_folder: str
+        :param config: An instance of SHConfig defining the service
+        :type config: SHConfig or None
+        :param max_threads: Maximum threads to be used when downloading data.
+        :type max_threads: int
+        :param mosaicking_order: Mosaicking order, which has to be either 
+            'mostRecent', 'leastRecent' or 'leastCC'.
+        :type mosaicking_order: str
+        :param aux_request_args: a dictionary with auxiliary information for 
+            the input_data part of the SH request
+        :type aux_request_args: dict
+        """
+        super().__init__(data_collection=data_collection, size=size, 
+                         resolution=resolution, cache_folder=cache_folder,
+                         config=config, max_threads=max_threads)
+
+        self.features = self._parse_and_validate_features(features)
+        self.responses = self._create_response_objects()
+
+        if not evalscript:
+            raise ValueError('evalscript parameter must not be missing/empty')
+        self.evalscript = evalscript
+
+        if maxcc and isinstance(maxcc, (int, float)) and (maxcc < 0 or maxcc > 1):
+            raise ValueError('maxcc should be a float on an interval [0, 1]')
+
+        self.maxcc = maxcc
+        self.time_difference = time_difference or dt.timedelta(seconds=1)
+        self.mosaicking_order = mosaicking_order
+        self.aux_request_args = aux_request_args
+
+    def _parse_and_validate_features(self, features):
+        if not features:
+            raise ValueError('features must be defined')
+
+        allowed_features = FeatureTypeSet.RASTER_TYPES.union({FeatureType.META_INFO})
+        _features = list(self._parse_features(features, 
+                                              allowed_feature_types=allowed_features, 
+                                              new_names=True)())
+
+        ftr_data_types = set(ft for ft, _, _ in _features if not ft.is_meta())
+        if all(ft.is_timeless() for ft in ftr_data_types) or\
+            all(ft.is_time_dependent() for ft in ftr_data_types):
+            return _features
+        raise ValueError('Cannot mix time dependent and timeless requests!')
+
+    def _create_response_objects(self):
+        """ 
+        Construct SentinelHubRequest output_responses from features
+        """
+        responses = []
+        for feat_type, feat_name, _ in self.features:
+            if feat_type.is_raster():
+                responses.append(SentinelHubRequest.output_response(feat_name, 
+                                                                    MimeType.TIFF))
+            elif feat_type.is_meta():
+                responses.append(SentinelHubRequest.output_response('userdata', 
+                                                                    MimeType.JSON))
+            else:
+                # should not happen as features have already been validated
+                raise ValueError(f'{feat_type} not supported!')
+        return responses
+
+    def _get_timestamp(self, time_interval, bbox):
+        """ 
+        Get the timestamp array needed as a parameter for downloading the images
+        """
+        if any(feat_type.is_timeless() for feat_type, _, _ in self.features if feat_type.is_raster()):
+            return None
+
+        return get_available_timestamps(bbox=bbox, time_interval=time_interval,
+                                        data_collection=self.data_collection,
+                                        maxcc=self.maxcc, 
+                                        time_difference=self.time_difference, 
+                                        config=self.config)
+
+    def _build_requests(self, bbox, size_x, size_y, timestamp, time_interval):
+        """ 
+        Build requests
+        """
+        if timestamp:
+            dates = [(date - self.time_difference, date + self.time_difference)\
+                     for date in timestamp]
+        else:
+            dates = [parse_time_interval(time_interval, allow_undefined=True)]\
+                if time_interval else [None]
+        return [self._create_sh_request(date, bbox, size_x, size_y) for date in dates]
+
+    def _create_sh_request(self, time_interval, bbox, size_x, size_y):
+        """ 
+        Create an instance of SentinelHubRequest
+        """
+        return SentinelHubRequest(
+            evalscript=self.evalscript,
+            input_data=[SentinelHubRequest.input_data(
+                data_collection=self.data_collection,
+                mosaicking_order=self.mosaicking_order,
+                time_interval=time_interval,
+                maxcc=self.maxcc,
+                other_args=self.aux_request_args
+            )],
+            responses=self.responses,
+            bbox=bbox,
+            size=(size_x, size_y),
+            data_folder=self.cache_folder,
+            config=self.config
+        )
+
+    def _extract_data(self, eopatch, data_responses, shape):
+        """ 
+        Extract data from the received images and assign them to eopatch features
+        """
+        if len(self.features) == 1:
+            ftype, fname, _ = self.features[0]
+            extension = 'json' if ftype.is_meta() else 'tif'
+            data_responses = [{f'{fname}.{extension}': data} for data in data_responses]
+
+        for ftype, fname, new_fname in self.features:
+            if ftype.is_meta():
+                data = [data['userdata.json'] for data in data_responses]
+
+            elif ftype.is_time_dependent():
+                data = np.asarray([data[f"{fname}.tif"] for data in data_responses])
+                data = data[..., np.newaxis] if data.ndim == 3 else data
+
+            else:
+                data = np.asarray(data_responses[0][f"{fname}.tif"])[..., np.newaxis]
+
+            eopatch[ftype][new_fname] = data
+        return eopatch
+
+
 
