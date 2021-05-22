39a3ad2f9afd62cee6c9f240bdf9d88792c11d7b
Nastasja <nastasja@live-eo.com>
2021-05-22 15:31:18 +0200

    added wkt to bbox

--- io/vector_IO.py
+++ io/vector_IO.py
@@ -6,6 +6,7 @@ Created on Sat May 22 15:21:20 2021
 """
 
 from pathlib import Path
+from sentinelhub import BBox, CRS
 
 def read_wkt_file(vector_path):
     import shapely.wkt
@@ -15,6 +16,27 @@ def read_wkt_file(vector_path):
     geometry = shapely.wkt.loads(shape)
     return shape, geometry
 
-path1 = "D:/Code/eotopia/tests/testdata/vectorfiles/theewaterskloof_dam_nominal.wkt"
+def wkt_to_bbox(vector_path, inflate_bbox=None):
+    """ 
+    inflate: float
+            e.g. 0.1
+    """
+    shape, geometry = read_wkt_file(vector_path)
+    minx, miny, maxx, maxy = geometry.bounds
+    
+    if not inflate_bbox:
+        inflate_bbox = 0.0
 
+    delx = maxx - minx
+    dely = maxy - miny
+    minx = minx - delx * inflate_bbox
+    maxx = maxx + delx * inflate_bbox
+    miny = miny - dely * inflate_bbox
+    maxy = maxy + dely * inflate_bbox
+    
+    bbox = BBox([minx, miny, maxx, maxy], crs=CRS.WGS84)
+    return bbox
+
+
+path1 = "D:/Code/eotopia/tests/testdata/vectorfiles/theewaterskloof_dam_nominal.wkt"
 shape, geometry = read_wkt_file(path1)
