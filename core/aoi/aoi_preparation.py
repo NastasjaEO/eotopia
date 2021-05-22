68b008bb8207d9d4f9e3e3ed9a60999a06c73015
Nastasja <nastasja@live-eo.com>
2021-05-22 14:42:06 +0200

    added splitting of aoi by bbox

--- core/aoi/aoi_preparation.py
+++ core/aoi/aoi_preparation.py
@@ -5,14 +5,19 @@ Created on Sat May 22 11:32:05 2021
 @author: freeridingeo
 """
 
+from pathlib import Path
+import numpy as np
 import geopandas as gpd
 from sentinelhub import BBoxSplitter, CRS
+from shapely.geometry import Polygon
 
 def basic_aoi_preparation(vectorfile_path, 
                           crs="EPSG:4326", buffer_size=None, res=None,
-                          gr_sz = None,
+                          gr_sz = None, split=False,
                           **kwargs):
     aoi = gpd.read_file(vectorfile_path)
+    path = Path(vectorfile_path)
+    outpath = path.home    
     
     inputs = []
     for arg in kwargs.values():
@@ -33,7 +38,7 @@ def basic_aoi_preparation(vectorfile_path,
     print(ShapeVal_a)
     print(ShapeVal_b)
 
-    if ShapeVal_a >= 10000:
+    if split == True:
         SplitVal_a = max(1, int(ShapeVal_a/1e4))
         SplitVal_b = max(1, int(ShapeVal_b/1e4))
         print('The extent of the AOI is {}m x {}m,\
@@ -41,6 +46,7 @@ def basic_aoi_preparation(vectorfile_path,
                                                               ShapeVal_b,
                                                               SplitVal_a,
                                                               SplitVal_b))
+        split_aoi_by_bbox(aoi, crs, ShapeVal_a, ShapeVal_b, outpath)
 
 
     if res != None:
@@ -56,6 +62,29 @@ def basic_aoi_preparation(vectorfile_path,
               .format(width_grid, heigth_grid))
 
     return aoi
+
+def split_aoi_by_bbox(aoi, crs, x_size, y_size, output_path):
+    aoi_shape = aoi.geometry.values[-1]
+
+    # split area of interest into an appropriate number of BBoxes
+    bbox_splitter = BBoxSplitter([aoi_shape], crs, (x_size, y_size))
+    bbox_list = np.array(bbox_splitter.get_bbox_list()) # get list of BBox geometries
+    info_list = np.array(bbox_splitter.get_info_list()) # get list of x (column) and y(row) indices
+
+    print(f'Each bounding box also has some info how it was created.\nExample:\n\
+          bbox: {bbox_list[0].__repr__()} \n info: {info_list[0]}\n')
+
+    geometry = [Polygon(bbox.get_polygon()) for bbox in bbox_list]
+    idxs_x = [info['index_x'] for info in info_list] # get column index for naming EOPatch
+    idxs_y = [info['index_y'] for info in info_list] # get row index for naming EOPatch
+
+    gdf = gpd.GeoDataFrame({'index_x': idxs_x, 'index_y': idxs_y},
+                       crs=crs.pyproj_crs(),
+                       geometry=geometry)
+
+    shapefile_name = output_path / 'BBoxes.geojson'
+    gdf.to_file(str(shapefile_name), driver="GeoJSON")
+
 
 path1 = "D:/Code/eotopia/tests/testdata/vectorfiles/small_test_aoi.shp"
 path2 = "D:/Code/eotopia/tests/testdata/vectorfiles/large_test_aoi.shp"
