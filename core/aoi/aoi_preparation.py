162f5b1e670c2c15567110c455d047943fd7a89c
Nastasja <nastasja@live-eo.com>
2021-05-22 11:45:05 +0200

    starting on aoi utilities

--- core/aoi/aoi_preparation.py
+++ core/aoi/aoi_preparation.py
@@ -7,10 +7,17 @@ Created on Sat May 22 11:32:05 2021
 
 import geopandas as gpd
 
-def basic_aoi_preparation(vectorfile_path, crs="EPSG:4326"):
+def basic_aoi_preparation(vectorfile_path, 
+                          crs="EPSG:4326", buffer_size=.001, 
+                          **kwargs):
     aoi = gpd.read_file(vectorfile_path)
+    
+    inputs = []
+    for arg in kwargs.values():
+        inputs.append(arg)
     
     if aoi.crs != crs:
         aoi = aoi.to_crs(crs)
     
-    print(aoi.crs)    
+    if "buffer "in inputs:
+        aoi = aoi.buffer(buffer_size)
