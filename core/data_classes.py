5ed854c038f3edb77a3a40680836f6f51978d6dd
Nastasja <nastasja@live-eo.com>
2021-05-24 10:31:22 +0200

    started on reprojection of raster

--- core/data_classes.py
+++ core/data_classes.py
@@ -820,3 +820,136 @@ class RasterData(object):
         Applies visualization parameters to an image.
         """
 
+def rasterize(vectorobject, reference, outname=None, burn_values=1, 
+              expressions=None, nodata=0, append=False):
+    """
+    rasterize a vector object
+    Parameters
+    ----------
+    vectorobject: Vector
+        the vector object to be rasterized
+    reference: Raster
+        a reference Raster object to retrieve geo information and extent from
+    outname: str or None
+        the name of the GeoTiff output file; if None, an in-memory object of type :class:`Raster` is returned and
+        parameter outname is ignored
+    burn_values: int or list
+        the values to be written to the raster file
+    expressions: list
+        SQL expressions to filter the vector object by attributes
+    nodata: int
+        the nodata value of the target raster file
+    append: bool
+        if the output file already exists, update this file with new rasterized values?
+        If True and the output file exists, parameters `reference` and `nodata` are ignored.
+    Returns
+    -------
+    Raster or None
+        if outname is `None`, a raster object pointing to an in-memory dataset else `None`
+    Example
+    -------
+    >>> outname1 = 'target1.tif'
+    >>> outname2 = 'target2.tif'
+    >>> with Vector('source.shp') as vec:
+    >>>     with Raster('reference.tif') as ref:
+    >>>         burn_values = [1, 2]
+    >>>         expressions = ['ATTRIBUTE=1', 'ATTRIBUTE=2']
+    >>>         rasterize(vec, reference, outname1, burn_values, expressions)
+    >>>         expressions = ["ATTRIBUTE2='a'", "ATTRIBUTE2='b'"]
+    >>>         rasterize(vec, reference, outname2, burn_values, expressions)
+    """
+    if expressions is None:
+        expressions = ['']
+    if isinstance(burn_values, (int, float)):
+        burn_values = [burn_values]
+    if len(expressions) != len(burn_values):
+        raise RuntimeError('expressions and burn_values of different length')
+
+    failed = []
+    for exp in expressions:
+        try:
+            vectorobject.layer.SetAttributeFilter(exp)
+        except RuntimeError:
+            failed.append(exp)
+    if len(failed) > 0:
+        raise RuntimeError('failed to set the following attribute' 
+                           'filter(s): ["{}"]'.format('", '.join(failed)))
+
+    if append and outname is not None and os.path.isfile(outname):
+        target_ds = rasterio.open(outname)
+    # TODO!
+    # else:
+    #     if outname is not None:
+    #         target_ds = gdal.GetDriverByName('GTiff').Create(outname, reference.cols, reference.rows, 1, gdal.GDT_Byte)
+    #     else:
+    #         target_ds = gdal.GetDriverByName('MEM').Create('', reference.cols, reference.rows, 1, gdal.GDT_Byte)
+    #     target_ds.SetGeoTransform(reference.raster.GetGeoTransform())
+    #     target_ds.SetProjection(reference.raster.GetProjection())
+    #     band = target_ds.GetRasterBand(1)
+    #     band.SetNoDataValue(nodata)
+    for expression, value in zip(expressions, burn_values):
+        vectorobject.layer.SetAttributeFilter(expression)
+    #     gdal.RasterizeLayer(target_ds, [1], vectorobject.layer, burn_values=[value])
+    # vectorobject.layer.SetAttributeFilter('')
+    # if outname is None:
+    #     return Raster(target_ds)
+    # else:
+    #     target_ds = None
+
+
+def reproject(rasterobject, reference, outname, targetres=None, 
+              resampling='bilinear', format='GTiff'):
+    """
+    reproject a raster file
+    Parameters
+    ----------
+    rasterobject: Raster or str
+        the raster image to be reprojected
+    reference: Raster, Vector, str, int or osr.SpatialReference
+        either a projection string or a spatial object with an attribute 'projection'
+    outname: str
+        the name of the output file
+    targetres: tuple
+        the output resolution in the target SRS; a two-entry tuple is required: (xres, yres)
+    resampling: str
+        the resampling algorithm to be used
+    format: str
+        the output file format
+    Returns
+    -------
+    """
+    if isinstance(rasterobject, str):
+        rasterobject = RasterData(rasterobject)
+
+    if not isinstance(rasterobject, RasterData):
+        raise RuntimeError('rasterobject must be of type Raster or str')
+    # TODO!
+    # if isinstance(reference, (Raster, Vector)):
+    #     projection = reference.projection
+    #     if targetres is not None:
+    #         xres, yres = targetres
+    #     elif hasattr(reference, 'res'):
+    #         xres, yres = reference.res
+    #     else:
+    #         raise RuntimeError('parameter targetres is missing and cannot be read from the reference')
+    # elif isinstance(reference, (int, str, osr.SpatialReference)):
+    #     try:
+    #         projection = crsConvert(reference, 'proj4')
+    #     except TypeError:
+    #         raise RuntimeError('reference projection cannot be read')
+    #     if targetres is None:
+    #         raise RuntimeError('parameter targetres is missing and cannot be read from the reference')
+    #     else:
+    #         xres, yres = targetres
+    else:
+        raise TypeError('reference must be of type Raster, Vector, osr.SpatialReference, str or int')
+    # TODO
+    # options = {'format': format,
+    #            'resampleAlg': resampling,
+    #            'xRes': xres,
+    #            'yRes': yres,
+    #            'srcNodata': rasterobject.nodata,
+    #            'dstNodata': rasterobject.nodata,
+    #            'dstSRS': projection}
+    # gdalwarp(rasterobject.raster, outname, options)
+
